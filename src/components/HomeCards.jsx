b4974e100d94c63a56cfd8b08c9305155445f851
Faiz Ahamed Shaik <143540804+Faizahamed-S@users.noreply.github.com>
2025-03-20 14:22:36 -0700

    Update Homacard.jsx

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -8,7 +8,8 @@ const HomeCards=()=>{
                 <Card>
                   <h2 className="text-2xl font-bold">For Developers</h2>
                   <p className="mt-2 mb-4">
-                    Browse our React jobs and start your career today
+                    Browse our React jobs and start your ca
+                    reer today
                   </p>
                   <a
                     href="/jobs.html"
